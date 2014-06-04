19814a309900391212d55c2c7eac6340a7bff445
Steve Kaliski <sjkaliski@gmail.com>
2014-06-04 12:50:40 -0400

    deployment
    fix
    
    try
    
    there we go
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    no tests

--- server/render.go
+++ server/render.go
@@ -6,6 +6,8 @@ import (
 	"html/template"
 	"io"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 var TEMPLATE_DIR string = "static"
@@ -16,7 +18,8 @@ func execute(name string, data interface{}) (*bytes.Buffer, error) {
 	tmplName := name + "-partial"
 
 	t := template.New(tmplName)
-	path := TEMPLATE_DIR + "/" + name + ".html"
+	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	path := dir + "/" + TEMPLATE_DIR + "/" + name + ".html"
 	buf, err := ioutil.ReadFile(path)
 	if err != nil {
 		panic(err)
@@ -54,7 +57,8 @@ func RenderTemplate(wr io.Writer, name string, data interface{}) error {
 		},
 	})
 
-	layoutPath := TEMPLATE_DIR + "/layout.html"
+	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	layoutPath := dir + "/" + TEMPLATE_DIR + "/layout.html"
 	buf, err := ioutil.ReadFile(layoutPath)
 	if err != nil {
 		return err
