c9ea7197d7617eabdff27369a62e1ac79c7b0f06
Steve Kaliski <sjkaliski@gmail.com>
2014-06-04 11:44:59 -0400

    deployment

---
+++ fabfile.py
@@ -0,0 +1,26 @@
+from fabric.api import *
+import requests
+
+project = "crosby"
+repository = "git@github.com:Bowery/" + project + ".git"
+crosby_hosts = [
+  'ec2-54-226-183-252.compute-1.amazonaws.com'
+]
+# env.key_filename = '/home/ubuntu/.ssh/id_aws'
+env.key_filename = '/Users/steve/.ssh/bowery.pem'
+env.password = 'java$cript'
+
+def restart_crosby():
+  with cd('/home/ubuntu/gocode/src/crosby'):
+    # run('git pull')
+    with cd('server'):
+      sudo('GOPATH=/home/ubuntu/gocode go get')
+      run('myth static/style.css static/out.css')
+
+    sudo('mv ../../bin/server /usr/local/bin/crosby')
+    sudo('cp -f scripts/crosby.conf /etc/init/crosby.conf')
+    sudo('initctl reload-configuration')
+    sudo('restart crosby')
+
+def crosby():
+  execute(restart_crosby, hosts=crosby_hosts)
